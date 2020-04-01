4c16b19586d65c6ed3d138d5902be8759856481a
JoeYen <JoeYen@via.com.tw>
2020-04-01 19:16:10 +0800

    1. using Material Calendar View to show week only. 2. replace listview to recyclerview.
    Signed-off-by: JoeYen <JoeYen@via.com.tw>

--- app/src/main/java/com/example/Financial/income.java
+++ app/src/main/java/com/example/Financial/income.java
@@ -1,40 +1,49 @@
 package com.example.Financial;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
+
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.CalendarView;
+
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class income extends Fragment {
     String TAG = this.getClass().getSimpleName();
-    final DateFormat mDateFormat = new SimpleDateFormat("yyyy/MM/dd");
     List<inComeInfo> mListIncomeInfo = new ArrayList<>();
-    adapter mAdapter = null;
+    dataAdapter mDataAdapter = null;
 
     // View
-    ListView mListView = null;
-    CalendarView mCalendarView = null;
+    RecyclerView mRecyclerView = null;
+    MaterialCalendarView mCalendarView = null;
     Button mBtnCheck = null;
     Button mBtnClear = null;
     EditText mEtRemark = null;
@@ -51,56 +60,66 @@ public class income extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         // get view
-        this.mCalendarView = getView().findViewById(R.id.calendarView);
+        this.mCalendarView = getView().findViewById(R.id.materialCalendarView);
         this.mBtnCheck = getView().findViewById(R.id.btn_check_income);
         this.mBtnClear = getView().findViewById(R.id.btn_clear_income);
         this.mEtAmount = getView().findViewById(R.id.editText_amount_income);
         this.mEtRemark = getView().findViewById(R.id.editText_remark_income);
         this.mSpinnerItems = getView().findViewById(R.id.spinner_items_income);
-        this.mListView = getView().findViewById(R.id.listview_income);
+        this.mRecyclerView = getView().findViewById(R.id.recyclerview_income);
 
         // set listener
-        this.mCalendarView.setOnDateChangeListener(dateChangeListener);
+        this.mCalendarView.setOnDateChangedListener(dateSelectedListener);
         this.mBtnCheck.setOnClickListener(btnCheckListener);
         this.mBtnClear.setOnClickListener(btnClearListener);
 
-        // set spinner
-        setSpinnerItems();
-
         // set list view
-        this.mAdapter = new adapter(this.getContext(), this.mListIncomeInfo);
-        this.mListView.setAdapter(this.mAdapter);
+        mDataAdapter = new dataAdapter(this.getContext(), this.mListIncomeInfo);
+        this.mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        this.mRecyclerView.setAdapter(mDataAdapter);
+
+        //
+        setSpinnerItems();
+        setCalendarView();
     }
 
-    CalendarView.OnDateChangeListener dateChangeListener = new CalendarView.OnDateChangeListener() {
+    OnDateLongClickListener dateLongClickListener = new OnDateLongClickListener() {
         @Override
-        public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-            // TODO : show SQL data as listview
-
-            mListIncomeInfo.clear();
+        public void onDateLongClick(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date) {
+            if( widget.getCalendarMode().equals(CalendarMode.MONTHS) )
+                widget.state().edit().setCalendarDisplayMode(CalendarMode.WEEKS).commit();
+            else
+                widget.state().edit().setCalendarDisplayMode(CalendarMode.MONTHS).commit();
         }
     };
 
-    View.OnClickListener btnCheckListener = new View.OnClickListener() {
+    OnDateSelectedListener dateSelectedListener = new OnDateSelectedListener() {
+        @Override
+        public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+            // TODO : show SQL data as listview
+            mListIncomeInfo.clear();
+            mDataAdapter.refresh();
+        }
+    };
+
+    final View.OnClickListener btnCheckListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if( mSpinnerItems.getSelectedItem() == null || mSpinnerItems.getSelectedItem().toString().isEmpty() ||
-                mEtAmount.getText() == null || mEtAmount.getText().toString().isEmpty() ||
-                mEtRemark.getText() == null || mEtRemark.getText().toString().isEmpty() )
-            {
+            if (mSpinnerItems.getSelectedItem() == null || mSpinnerItems.getSelectedItem().toString().isEmpty() ||
+                    mEtAmount.getText() == null || mEtAmount.getText().toString().isEmpty() ||
+                    mEtRemark.getText() == null || mEtRemark.getText().toString().isEmpty()) {
                 Toast.makeText(getContext(), "Please key in full info.", Toast.LENGTH_LONG).show();
                 return;
             }
 
             // TODO : add data to SQL
             inComeInfo inComeInfo = new inComeInfo();
-            inComeInfo.mDate = mDateFormat.format(mCalendarView.getDate());
+            inComeInfo.mDate = mCalendarView.getSelectedDate().getDate().toString();
             inComeInfo.mItem = mSpinnerItems.getSelectedItem().toString();
             inComeInfo.mAmount = mEtAmount.getText().toString();
             inComeInfo.mRemark = mEtRemark.getText().toString();
             mListIncomeInfo.add(inComeInfo);
-
-            mAdapter.refresh(mListIncomeInfo);
+            mDataAdapter.refresh();
         }
     };
 
@@ -121,8 +140,13 @@ public class income extends Fragment {
     }
 
     private void setCalendarView(){
-        // TODO : set calendar view as one week only
-        // https://github.com/Tibolte/AgendaCalendarView
+        // set current date
+        this.mCalendarView.setSelectedDate(this.mCalendarView.getCurrentDate());
+
+        // set calendar view as one week only
+        this.mCalendarView.state().edit().setCalendarDisplayMode(CalendarMode.WEEKS).commit();
+
+        this.mCalendarView.setOnDateLongClickListener(dateLongClickListener);
     }
 
     // =================================================================
@@ -133,74 +157,57 @@ public class income extends Fragment {
         public String mAmount;
         public String mRemark;
 
-        public inComeInfo(){ };
+        public inComeInfo(){ }
     }
 
     // =================================================================
 
-    private class adapter extends BaseAdapter {
-        // TODO remove recycle
-        // http://hulkyang.blogspot.com/2019/01/recyclerview-itemtouchhelper.html
+    private class dataAdapter extends RecyclerView.Adapter<dataAdapter.ViewHolder>{
+        // https://codeburst.io/android-swipe-menu-with-recyclerview-8f28a235ff28
 
-        private ViewHolder mViewHolder = null;
-        private LayoutInflater mLayoutInflater = null;
-        private Context mContext = null;
-        List<inComeInfo> mList = new ArrayList<>();
+        List<inComeInfo> mList;
+        private Context mContext;
 
-        class ViewHolder{
-            TextView textviewItem = null;
-            TextView textviewAmount = null;
-            TextView textviewRemark = null;
+        class ViewHolder extends RecyclerView.ViewHolder{
+            TextView textviewItem, textviewAmount, textviewRemark;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                this.textviewItem = itemView.findViewById(R.id.textView_item);
+                this.textviewAmount = itemView.findViewById(R.id.textView_amount);
+                this.textviewRemark = itemView.findViewById(R.id.textView_remark);
+            }
         }
 
-        adapter(Context context, List<inComeInfo> list){
+        public dataAdapter(Context context, List<inComeInfo> list) {
+            super();
+
             this.mList = list;
             this.mContext = context;
-            this.mLayoutInflater = LayoutInflater.from(context);
+        }
+
+        @NonNull
+        @Override
+        public dataAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listview_income, parent, false);
+            return new ViewHolder(view);
         }
 
         @Override
-        public int getCount() {
+        public void onBindViewHolder(@NonNull dataAdapter.ViewHolder holder, int position) {
+            inComeInfo inComeInfo = this.mList.get(position);
+            holder.textviewItem.setText(inComeInfo.mItem);
+            holder.textviewAmount.setText(inComeInfo.mAmount);
+            holder.textviewRemark.setText(inComeInfo.mRemark);
+        }
+
+        @Override
+        public int getItemCount() {
             return this.mList.size();
         }
 
-        @Override
-        public Object getItem(int i) {
-            return this.mList.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            // create item
-            mViewHolder = null;
-            if(view == null){
-                view = mLayoutInflater.inflate(R.layout.listview_income, null);
-                mViewHolder = new ViewHolder();
-                mViewHolder.textviewItem = view.findViewById(R.id.textView_item);
-                mViewHolder.textviewRemark = view.findViewById(R.id.textView_remark);
-                mViewHolder.textviewAmount = view.findViewById(R.id.textView_amount);
-                view.setTag(mViewHolder);
-            }
-            else{
-                mViewHolder = (ViewHolder) view.getTag();
-            }
-
-            // set item ui
-            inComeInfo inComeInfo = (inComeInfo) getItem(i);
-            mViewHolder.textviewItem.setText(inComeInfo.mItem);
-            mViewHolder.textviewRemark.setText(inComeInfo.mRemark);
-            mViewHolder.textviewAmount.setText(inComeInfo.mAmount);
-
-            return view;
-        }
-
-        public void refresh(List<inComeInfo> list){
-            notifyDataSetChanged();
+        public void refresh(){
+            this.notifyDataSetChanged();
         }
     }
 }
